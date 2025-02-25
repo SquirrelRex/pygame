85bb0380ebe2080a5da07bf2a9b215013cfad7c0
SquirrelRex <leftseat12@pm.me>
2025-02-24 23:16:29 -0700

    laser spaced out

--- player.py
+++ player.py
@@ -5,8 +5,8 @@ from constants import PLAYER_TURN_SPEED
 from constants import PLAYER_SPEED
 from constants import SHOT_RADIUS
 from constants import PLAYER_SHOT_SPEED
+from constants import PLAYER_SHOOT_COOLDOWN
 from shot import Shot
-
 
 class Player(CircleShape):
     #stuff goes here
@@ -16,6 +16,7 @@ class Player(CircleShape):
         self.x = x
         self.y = y
         self.radius = PLAYER_RADIUS
+        self.timer = 0
 
 
     def triangle(self):
@@ -46,13 +47,15 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_SPACE]:
             self.shoot()
+        self.timer -= dt
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
-        shot = Shot(self.position.x, self.position.y)
-        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOT_SPEED
-    
-
+        if self.timer <= 0:
+            self.timer += PLAYER_SHOOT_COOLDOWN
+            shot = Shot(self.position.x, self.position.y)
+            shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOT_SPEED
+            
